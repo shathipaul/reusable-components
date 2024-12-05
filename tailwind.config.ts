63e0189196da35803a3475e5e02b949d3e4da603
shathipaul <shathipaul11@gmail.com>
2024-12-05 19:38:00 +0600

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,45 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      keyframes: {
+        zoomIn: {
+          "0%": {
+            opacity: 0,
+            transform: "scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        zoomIn: "zoomIn 0.5s ease-in-out forwards",
+      },
+    },
+    // our colors theme
+    colors: {
+      primary: "#262826",
+      secondary: "#6E717A",
+      black: "#000000",
+      white: "#ffffff",
+      offWhite: "#FAF9F6",
+      catSkillWhite: "#E5E7E2",
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        xxl: "1400px",
       },
     },
   },
+
   plugins: [],
-} satisfies Config;
+};
